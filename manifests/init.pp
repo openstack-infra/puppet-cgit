2f27879e4c3df568ef9aa079f2da5a4185d4415b
Timothy Chavez <timothy.chavez@hp.com>
2015-08-19 10:15:37 -0500

    Do not execute selinux commands if policy disabled
    If the system running cgit has disabled selinux, cgit should not attempt
    to run any selinux commands to prevent puppet apply from failing.
    
    Change-Id: I21add092d9d09077f2b23760a384f5a5cb91d86a

--- manifests/init.pp
+++ manifests/init.pp
@@ -31,7 +31,8 @@ class cgit(
   $manage_cgitrc = false,
   $prefork_settings = {}, # override the prefork worker settings
   $mpm_settings = {}, # override the mpm worker settings
-  $cgitrc_settings = {}
+  $cgitrc_settings = {},
+  $selinux_mode = 'enforcing'
 ) {
   validate_hash($prefork_settings)
   validate_hash($mpm_settings)
@@ -92,7 +93,9 @@ class cgit(
   include ::httpd
 
   if ($::osfamily == 'RedHat') {
-    include ::cgit::selinux
+    if $selinux_mode != 'disabled' {
+        include ::cgit::selinux
+    }
   }
 
   package { [
